a5168151a0810f99a19a145d59aab7bf5c23f250
Nikita Vetyugov <nickitavet1998@gmail.com>
2022-06-27 21:29:19 +0300

    Initial commit

---
+++ src/test/java/com/geekbrains/neon/NewBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.neon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
